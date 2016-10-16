92fe7726924992d142ba327919ae3d8e8dab841e
Andi Mullaraj <andimullaraj@gmail.com>
2016-10-16 15:27:30 -0700

    Removed the server provider -- it would add confusion in the longer run. Don't think it is a common use case.

--- rouplex-jersey-application/src/main/java/org/rouplex/service/SecurityServiceJerseyApplication.java
+++ rouplex-jersey-application/src/main/java/org/rouplex/service/SecurityServiceJerseyApplication.java
@@ -1,26 +1,17 @@
 package org.rouplex.service;
 
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
-import org.glassfish.jersey.process.internal.RequestScoped;
 import org.rouplex.service.securityservice.SecurityServiceJerseyResource;
-import org.rouplex.service.securityservice.SecurityServiceServer;
-import org.rouplex.service.securityservice.SecurityServiceServerFactory;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Context;
-import java.util.Arrays;
 
 @ApplicationPath(SecurityServiceJerseyApplication.APP_PATH)
 public class SecurityServiceJerseyApplication extends RouplexJerseyApplication {
-    public SecurityServiceJerseyApplication(@Context ServletContext servletContext) {
-        super(servletContext, Arrays.asList(SecurityServiceJerseyResource.class));
 
-        register(new AbstractBinder() {
-            @Override
-            protected void configure() {
-                bindFactory(SecurityServiceServerFactory.class).to(SecurityServiceServer.class).in(RequestScoped.class);
-            }
-        });
+    public SecurityServiceJerseyApplication(@Context ServletContext servletContext) {
+        super(servletContext);
+
+        registerRouplexResource(SecurityServiceJerseyResource.class, true);
     }
 }
--- rouplex-security-service-jersey-resource/src/main/java/org/rouplex/service/securityservice/SecurityServiceJerseyResource.java
+++ rouplex-security-service-jersey-resource/src/main/java/org/rouplex/service/securityservice/SecurityServiceJerseyResource.java
@@ -5,20 +5,27 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.rouplex.jaxrs.security.RouplexSecurityContext;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Context;
 
 @Api(value = "/security", description = "SecurityService resource")
 public class SecurityServiceJerseyResource extends ResourceConfig implements SecurityService {
     @Context
-    SecurityServiceServer securityServiceServer;
+    HttpServletRequest httpServletRequest;
+    @Context
+    RouplexSecurityContext rouplexSecurityContext;
+    @Context
+    HttpServletResponse httpServletResponse;
 
     @ApiOperation(value = "Responds back with PingResponse, providing routing and plexing info")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success"),
             @ApiResponse(code = 404, message = "Not found") })
     public PingResponse ping() {
-        return securityServiceServer.ping();
+        return getSecurityServiceServer().ping();
     }
 
     @ApiOperation(value = "Responds back with PingResponse, providing routing and plexing info")
@@ -27,6 +34,10 @@ public class SecurityServiceJerseyResource extends ResourceConfig implements Sec
             @ApiResponse(code = 400, message = "Invalid request"),
             @ApiResponse(code = 404, message = "Not found") })
     public PingResponse ping(String payload) {
-        return securityServiceServer.ping(payload);
+        return getSecurityServiceServer().ping(payload);
+    }
+
+    private SecurityServiceServer getSecurityServiceServer() {
+        return new SecurityServiceServer(httpServletRequest, rouplexSecurityContext, httpServletResponse);
     }
 }
