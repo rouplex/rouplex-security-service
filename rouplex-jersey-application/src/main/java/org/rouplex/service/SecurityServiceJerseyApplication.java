91e0c05308b642d3f97ece6716353cde2ec0fb29
Andi Mullaraj <andimullaraj@gmail.com>
2017-02-16 03:15:39 -0800

    Adapting to jersey-binder

--- rouplex-jersey-application/src/main/java/org/rouplex/service/SecurityServiceJerseyApplication.java
+++ rouplex-jersey-application/src/main/java/org/rouplex/service/SecurityServiceJerseyApplication.java
@@ -1,17 +1,18 @@
 package org.rouplex.service;
 
+import org.rouplex.platform.jersey.RouplexApplication;
 import org.rouplex.service.securityservice.SecurityServiceJerseyResource;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Context;
 
-@ApplicationPath(SecurityServiceJerseyApplication.APP_PATH)
-public class SecurityServiceJerseyApplication extends RouplexJerseyApplication {
+@ApplicationPath("/rouplex")
+public class SecurityServiceJerseyApplication extends RouplexApplication {
 
     public SecurityServiceJerseyApplication(@Context ServletContext servletContext) {
         super(servletContext);
 
-        registerRouplexResource(SecurityServiceJerseyResource.class, true);
+        binder.bindResource(SecurityServiceJerseyResource.class, true);
     }
 }
