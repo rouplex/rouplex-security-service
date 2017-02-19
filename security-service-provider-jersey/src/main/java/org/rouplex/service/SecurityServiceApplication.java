b509d4cf51461fbb56a1adaf9b27fbdf5956c7c0
Andi Mullaraj <andimullaraj@gmail.com>
2017-02-18 22:30:50 -0800

    Simplified the project structure and put together the app and service, removed rouplex as prefix

--- security-service-provider-jersey/src/main/java/org/rouplex/service/SecurityServiceApplication.java
+++ security-service-provider-jersey/src/main/java/org/rouplex/service/SecurityServiceApplication.java
@@ -7,6 +7,11 @@ import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Context;
 
+/**
+ * This is the webapp, or the main jersey {@link javax.ws.rs.core.Application} which binds all the jersey resources.
+ * The container searches for the {@link ApplicationPath} annotation and instantiates an instance of this class. It is
+ * only in the constructor that we can bind (or add) resources to it, the jersey API does not allow for anything else.
+ */
 @ApplicationPath("/rouplex")
 public class SecurityServiceApplication extends RouplexApplication {
 
